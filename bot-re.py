24e9c3d9d271e0eff488ae44d3caca5d959e4d23
Lapsus134 <136785099+Lapsus134@users.noreply.github.com>
2023-06-18 15:09:23 +0330

    Add files via upload

---
+++ bot-re.py
@@ -0,0 +1,35 @@
+import requests
+import random
+import string
+import time
+
+
+num = 1000000
+
+with open("Nitro Codes.txt", "w", encoding='utf-8') as file:
+
+    start = time.time()
+
+    for i in range(num):
+        code = "".join(random.choices(
+            string.ascii_uppercase + string.digits + string.ascii_lowercase,
+            k = 16
+        ))
+
+        file.write(f"https://discord.gift/{code}\n")
+
+    print(f"Generated {num} codes | Time taken: {time.time() - start}\n")
+
+with open("Nitro Codes.txt") as file:
+    for line in file.readlines():
+        nitro = line.strip("\n")
+
+        url = "https://discordapp.com/api/v6/entitlements/gift-codes/" + nitro + "?with_application=false&with_subscription_plan=true"
+
+        r = requests.get(url)
+
+        if r.status_code == 200:
+            print(f" Valid | {nitro} ")
+            break
+        else:
+            print(f" Invalid | {nitro} ")
